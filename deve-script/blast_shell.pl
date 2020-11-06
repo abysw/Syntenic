80679b399a9e51528bce564e0af6c523748f847e
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 19:56:33 +0800

    Update blast_shell.pl

--- deve-script/blast_shell.pl
+++ deve-script/blast_shell.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 =head1 Name
-    /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/blast_shell.pl
+    blast_shell.pl
 Info
     Version Perl-pipline.1.0
-    Author: st_agric (FuYuan), fuyuan@genomics.cn
+    Author: Yuan-SW-F, yuanswf@163.com
     Created Time: 2017-10-31 20:07:51
     Created Version: blast_shell.pl
 Usage
@@ -11,7 +11,7 @@ Usage
 =cut
 use strict;
 use feature qw(say);
-use lib '/home/st_agric/lib';
+#use lib '/home/***/lib';
 use MCsub;
 use Getopt::Long;
 my ($query,$database,$help);
@@ -28,9 +28,9 @@ my $file=$1 if $query=~/([^\/\.]+)[^\/]+$/;
 $file.="\-$1" if $database=~/([^\/\.]+)[^\/]+$/;
 
 mkdir $file;
-output("$file/step1_build.sh","/hwfssz1/ST_AGRIC/USER/heshixu/Program/software/ncbi-blast-2.6.0+/bin/makeblastdb -in $database -dbtype prot -input_type fasta");
-output("$file/step2_blastp.sh","/hwfssz1/ST_AGRIC/USER/heshixu/Program/software/ncbi-blast-2.6.0+/bin/blastp -num_threads 5 -db $database -query $query -evalue 1e-10 -outfmt 6 -out $file.blastp -num_alignments 5");
-output("$file/step3_filter.sh","perl /hwfssz1/ST_AGRIC/USER/heshixu/Program/script/comparative_genomics/blast_filter.pl -r 5 -self -e 1e-10 -o $file.besthit.blast $file.blastp");
+output("$file/step1_build.sh","makeblastdb -in $database -dbtype prot -input_type fasta");
+output("$file/step2_blastp.sh","blastp -num_threads 5 -db $database -query $query -evalue 1e-10 -outfmt 6 -out $file.blastp -num_alignments 5");
+output("$file/step3_filter.sh","blast_filter.pl -r 5 -self -e 1e-10 -o $file.besthit.blast $file.blastp");
 output("$file/work.sh","sh step2_blastp.sh;\nsh step3_filter.sh;\n");
 #my $pipath=path("Perl-pipline.1.0"); #$pipath/script/config.txt
 
