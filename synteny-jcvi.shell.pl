4ed113ddf5346909964aab6e06bb4f8b750a253b
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 16:21:03 +0800

    Add files via upload

---
+++ synteny-jcvi.shell.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl -w
+=head1 Name
+    /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/synteny-jcvi.shell.pl
+Info
+    Version Perl-pipline.1.0
+    Author: st_agric (FuYuan), fuyuan@genomics.cn
+    Created Time: 2018-04-27 16:00:15
+    Created Version: synteny-jcvi.shell.pl
+Usage
+    synteny-jcvi.shell.pl	
+=cut
+use strict;
+use feature qw(say);
+use lib '/home/st_agric/lib';
+use agric;
+use Getopt::Long;
+my ($help,$force,$second,$sp1,$sp2,$cds1,$cds2,$gff1,$gff2,$out,$minspan);
+GetOptions(
+	"sp1:s"=>\$sp1,
+	"sp2:s"=>\$sp2,
+	"cds1:s"=>\$cds1,
+	"cds2:s"=>\$cds2,
+	"gff1:s"=>\$gff1,
+	"gff2:s"=>\$gff2,
+	"out:s"=>\$out,
+	"second|s!"=>\$second,
+	"force!"=>\$force,
+	"minspan:i"=>\$minspan,
+	"help!"=>\$help
+);
+$cds1||=shift;
+$cds2||=shift;
+$minspan||=30;
+my $bin="/public/agis/chengshifeng_group/fuyuan/pip-fuyuan/06.synteny/script";
+die "please prepare the cds files\nor run python -m jcvi.apps.fetch phytozome sp1,sp2" if -z $cds1;
+die "please prepare the cds files\nor run python -m jcvi.apps.fetch phytozome sp1,sp2" if -z $cds2;
+$cds1="./$cds1" if $cds1!~/^\//;
+$cds1=~/(\S+\/([A-Za-z]+\_[A-Za-z]+)\w*)[^\/]+$/;
+$sp1 =$2;
+$gff1=$1 if `ls $1*gff*`=~/(\S+)/;
+$cds2="./$cds2" if $cds2!~/^\//;
+$cds2=~/(\S+\/([A-Za-z]+\_[A-Za-z]+)\w*)[^\/]+$/;
+$sp2 =$2;
+$gff2=$1 if `ls $1*gff*`=~/(\S+)/;
+
+$out||="$sp1-$sp2";
+mkdir $out if ! -f $out;
+#die "please set --force " if (! -f $cds1 && ! -f $cds2 );
+my $pwd=$1 if `pwd`=~/(\S+)/;
+`less $cds1 |perl -ne 's/(\>\\S+).*/\$1/;print \$_' > $pwd/$out/$sp1\_raw.cds`;
+#`less $cds1 |perl -ne 's/(\>[^\.]+).*/\$1/;print \$_' > $pwd/$out/$sp1\_raw.cds`;
+$cds1="$pwd/$out/$sp1\_raw.cds";
+`less $cds2 |perl -ne 's/(\>\\S+).*/\$1/;print \$_' > $pwd/$out/$sp2\_raw.cds`;
+#`less $cds2 |perl -ne 's/(\>[^\.]+).*/\$1/;print \$_' > $pwd/$out/$sp2\_raw.cds`;
+$cds2="$pwd/$out/$sp2\_raw.cds";
+`less $gff1 |perl -ne 's/(=\\S+).*/\$1/;print \$_' > $pwd/$out/$sp1\_raw.gff`;
+#`less $gff1 |perl -ne 's/(=[^\.]+).*/\$1/;print \$_' > $pwd/$out/$sp1\_raw.gff`;
+$gff1="$pwd/$out/$sp1\_raw.gff";
+`less $gff2 |perl -ne 's/(=\\S+).*/\$1/;print \$_' > $pwd/$out/$sp2\_raw.gff`;
+#`less $gff2 |perl -ne 's/(=[^\.]+).*/\$1/;print \$_' > $pwd/$out/$sp2\_raw.gff`;
+$gff2="$pwd/$out/$sp2\_raw.gff";
+my $change=  $sp1 eq $sp2 ? "-ch" : ""; 
+die opth() if $help;
+
+#my $pipath=path("Perl-pipline.1.0"); #$pipath/script/config.txt
+
+output("$out/synteny-jcvi-setp1.sh","#!/bin/bash
+source ~/.bashrc
+export PATH=/public/agis/chengshifeng_group/fuyuan/pip-fuyuan/local/bin:\$PATH
+export PATH=/public/software/gcc-6.4.0/bin:\$PATH
+export LD_LIBRARY_PATH=/public/software/gcc-6.4.0/lib64:\$LD_LIBRARY_PATH
+#python -m jcvi.apps.fetch phytozome $sp1,$sp2
+#mkdir synteny-jcvi ; cd synteny-jcvi ;
+python -m jcvi.formats.gff bed --type=mRNA --key=ID $gff1 -o $sp1.bed
+python -m jcvi.formats.gff bed --type=mRNA --key=ID $gff2 -o $sp2.bed
+python -m jcvi.formats.fasta format --sep=\"\|\" $cds1 $sp1.cds
+python -m jcvi.formats.fasta format --sep=\"\|\" $cds2 $sp2.cds
+
+python -m jcvi.compara.catalog ortholog $sp1 $sp2 > ../synteny.log");
+
+output("$out/synteny-jcvi.sh","#!/bin/bash
+source ~/.bashrc
+export PATH=/public/agis/chengshifeng_group/fuyuan/pip-fuyuan/local/bin:\$PATH
+export PATH=/public/software/gcc-6.4.0/bin:\$PATH
+export LD_LIBRARY_PATH=/public/software/gcc-6.4.0/lib64:\$LD_LIBRARY_PATH
+#python -m jcvi.apps.fetch phytozome $sp1,$sp2
+mkdir synteny-jcvi ; cd synteny-jcvi ;
+python -m jcvi.formats.gff bed --type=mRNA --key=ID $gff1 -o $sp1.bed
+python -m jcvi.formats.gff bed --type=mRNA --key=ID $gff2 -o $sp2.bed
+python -m jcvi.formats.fasta format --sep=\"\|\" $cds1 $sp1.cds
+python -m jcvi.formats.fasta format --sep=\"\|\" $cds2 $sp2.cds
+
+python -m jcvi.compara.catalog ortholog $sp1 $sp2 > ../synteny.log
+python -m jcvi.graphics.dotplot $sp1.$sp2.anchors
+python -m jcvi.compara.synteny depth --histogram $sp1.$sp2.anchors
+python -m jcvi.compara.synteny screen --minspan=$minspan --simple $sp1.$sp2.anchors $sp1.$sp2.anchors.new
+### sort
+perl $bin/anchors_resort_shell.pl --num 30 --bed1 $sp1.bed --bed2 $sp2.bed --anch $sp1.$sp2.anchors $change -b 
+python -m jcvi.graphics.dotplot $sp1\_s1.$sp2\_s2.anchors --nosort
+mv $sp1\_s1.$sp2\_s2.pdf $sp1\_s1.$sp2\_s2-b.pdf
+perl /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/anchors_resort_shell.pl --num 30 --bed1 $sp1.bed --bed2 $sp2.bed --anch $sp1.$sp2.anchors $change 
+python -m jcvi.graphics.dotplot $sp1\_s1.$sp2\_s2.anchors --nosort
+mv $sp1\_s1.$sp2\_s2.pdf $sp1\_s1.$sp2\_s2-l.pdf
+python -m jcvi.graphics.dotplot $sp1\-1.$sp2\-2.anchors --nosort
+python -m jcvi.graphics.karyotype seqids layout
+cp *pdf ..
+perl $bin/synteny-jcvi_stat.pl synteny.log
+python -m jcvi.compara.synteny mcscan $sp1.bed $sp1.$sp2.lifted.anchors --iter=1 -o $sp1.$sp2.i1.blocks
+head -50 $sp1.$sp2.i1.blocks > blocks
+#blocks.layout   ### fill in 
+cat $sp1.bed $sp2.bed > $sp1\_$sp2.bed
+#python -m jcvi.graphics.synteny blocks $sp1\_$sp2.bed blocks.layout
+echo \"\# x,   y, rotation,   ha,     va,   color, ratio,            label\n0.5, 0.6,        0, left, center,       m,     1,       $sp1 id1\n0.5, 0.4,        0, left, center, #fc8d62,     1, $sp2 id2\n# edges\ne, 0, 1\" > blocks.layout 
+python -m jcvi.graphics.synteny blocks $sp1\_$sp2.bed blocks.layout
+
+mkdir ../second ; cd ../second ;
+ln -s ../synteny-jcvi/$sp1.bed ../synteny-jcvi/$sp2.bed ../synteny-jcvi/$sp1.cds ../synteny-jcvi/$sp2.cds . 
+python -m jcvi.compara.catalog ortholog $sp1 $sp2 --cscore=.99 synteny.log2
+python -m jcvi.graphics.dotplot $sp1.$sp2.anchors
+python -m jcvi.compara.synteny depth --histogram $sp1.$sp2.anchors
+python -m jcvi.compara.synteny screen --minspan=$minspan --simple $sp1.$sp2.anchors $sp1.$sp2.anchors.new
+### sort
+perl $bin/anchors_resort_shell.pl --num 30 --bed1 $sp1.bed --bed2 $sp2.bed --anch $sp1.$sp2.anchors $change -b
+python -m jcvi.graphics.dotplot $sp1\_s1.$sp2\_s2.anchors --nosort
+mv $sp1\_s1.$sp2\_s2.pdf $sp1\_s1.$sp2\_s2-b.pdf
+perl $bin/anchors_resort_shell.pl --num 30 --bed1 $sp1.bed --bed2 $sp2.bed --anch $sp1.$sp2.anchors $change
+python -m jcvi.graphics.dotplot $sp1\_s1.$sp2\_s2.anchors --nosort
+mv $sp1\_s1.$sp2\_s2.pdf $sp1\_s1.$sp2\_s2-l.pdf
+python -m jcvi.graphics.dotplot $sp1\-1.$sp2\-2.anchors --nosort
+cp $sp1.$sp2.pdf ../$sp1.$sp2-fin.pdf
+cp $sp1.$sp2.depth.pdf ../$sp1.$sp2.depth-fin.pdf
+cp $sp1-1.$sp2-2.pdf ../$sp1-1.$sp2-2-fin.pdf
+cp $sp1\_s1.$sp2\_s2-b.pdf ../$sp1\_s1.$sp2\_s2-b-fin.pdf
+cp $sp1\_s1.$sp2\_s2-l.pdf ../$sp1\_s1.$sp2\_s2-l-fin.pdf
+
+python -m jcvi.graphics.karyotype seqids ../layout
+ perl $bin/synteny-jcvi_stat.pl synteny.log2
+");
+#output("$out/synteny-jcvi2.sh", if $second;
+output("$out/layout","# y, xstart, xend, rotation, color, label, va,  bed
+ .6,	.1,	.8,	0,	, $sp1, top, $sp1-1.bed
+ .4,	.1,	.8,	0,	, $sp2, top, $sp2-2.bed
+# edges
+e, 0, 1, $sp1.$sp2.anchors.simple
+")
+
+######################### Sub Routines #########################
---
+++ synteny-jcvi_stat.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+=head1 Name
+    /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/synteny-jcvi_stat.pl
+Info
+    Version Perl-pipline.1.0
+    Author: st_agric (FuYuan), fuyuan@genomics.cn
+    Created Time: 2018-05-04 10:06:43
+    Created Version: /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/synteny-jcvi_stat.pl
+Usage
+    /hwfssz1/ST_AGRIC/LOCAL/Pipline/Perl-pipline.1.0/06.synteny/bin/synteny-jcvi_stat.pl	
+=cut
+use strict;
+use feature qw(say);
+use lib '/home/st_agric/lib';
+use agric;
+use Getopt::Long;
+my ($help);
+GetOptions(
+	"help!"=>\$help
+);
+die opth() if $help;
+
+#my $pipath=path("Perl-pipline.1.0"); #$pipath/script/config.txt
+my $file=shift;
+my $list=`cat $file`;
+#my $text=$1 if `cat $file`=~/\[base\]\s+Load\s+file\s+\`(\w+\.\w+)\.lifted\.anchors\`\s+A\s+total\s+of\s+(\d+)\s+\(NR\:(\d+))\s+anchors\s+found\s+in\s+(\d+)\s+clusters\.\s+Stats\:\s+(Min=4 Max=101 N=1068 Mean=13.8380149813 SD=10.7344948102 Median=11.0 Sum=14779\nNR stats: Min=4 Max=57 N=1068 Mean=8.39138576779 SD=5.14748772848 Median=7.0 Sum=8962\n\S+\s+[synteny]/
+my @line= split /\n/,$list;
+my $i=0;
+my $stat;
+my $nr;
+my $head;
+while ($i<$#line){
+	if ($line[$i]=~/\[base\].+\`(\w+\.\w+)\.anchors\`/){
+		open OUT , ">$1.stat";
+		$head=$1;
+		$stat=$line[$i+2];
+		$nr=$line[$i+3];
+	say $line[$i+1];
+	last;
+	}
+	$i++;
+}
+my (@id,@v1,@v2);
+my @stat= split /\s+/ ,$stat;
+for (@stat[1..$#stat]){
+	/(\w+)\=(\S+)/;
+	push @id,$1;
+	push @v1,$2;
+}
+my @nr= split /\s+/ ,$nr;
+for (@nr[2..$#nr]){
+	/(\w+)\=(\S+)/;
+	push @v2,$2;
+}
+$head="$1-$2" if $head=~/(\w\w\w)[a-z]+\.(\w\w\w)/;
+say OUT join "\t","$head",@id;
+say OUT join "\t","stat",@v1;
+say OUT join "\t","NR",@v2;
+######################### Sub Routines #########################
