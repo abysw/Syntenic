9d742b88ee458c95aacec6c541b0ceb2df0c6c97
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 19:57:14 +0800

    Update block-t3-plot.py

--- deve-script/block-t3-plot.py
+++ deve-script/block-t3-plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Author: fuyuan (907569282@qq.com)
+# Author: Yuan-SW-F, yuanswf@163.com
 # Created Time: 2019-07-05 09:13:49
 # Example block.py   
 import sys, os, re
